2fe46c1fe709ce3f4f58df079ffe32420ccea12a
Shafayetul Islam <shafatsib@gmail.com>
2025-10-09 05:02:08 +0600

    changed: arrow key

--- speaking/speaking.js
+++ speaking/speaking.js
@@ -94,8 +94,8 @@
         <div class="sp-head">
           <select id="sp-story" title="Choose story"></select>
           <select id="sp-voice" title="Voice (ja-JP preferred)"></select>
-          <button id="sp-prev"  type="button">⟵ Prev</button>
-          <button id="sp-next"  type="button">Next ⟶</button>
+          <button id="sp-prev"  type="button">↑ Prev</button>
+          <button id="sp-next"  type="button">Next ↓</button>
           <button id="sp-rate-down" type="button">− Speed</button>
           <span   id="sp-rate-badge" class="rate-badge">1.00×</span>
           <button id="sp-rate-up" type="button">+ Speed</button>
